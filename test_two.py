80fbedd6c7839569cc9eaee6e0f5292a0d592400
Rabbbint <150071058+Rabbbint@users.noreply.github.com>
2024-05-31 23:38:24 +0300

    correction_1

--- test_two.py
+++ test_two.py
@@ -1,12 +1,15 @@
 import unittest
-from two import roman_to_arabic
+from random import randint
+from two import rand_list
 
+class TestRandList(unittest.TestCase):
 
-class TestRomanToArabic(unittest.TestCase):
-    def test_common_prefix(self):
-        self.assertEqual(roman_to_arabic("CLIV"), 154)
-        self.assertEqual(roman_to_arabic("XI"), 11)
-
+    def test_rand_list(self):
+        a = [randint(0, 1001) for _ in range(10)]
+        original_a = a.copy()
+        deleted_num = rand_list(a)
+        self.assertIn(deleted_num, original_a)
+        self.assertEqual(len(a), len(original_a) - 1)
 
 if __name__ == '__main__':
     unittest.main()
--- two.py
+++ two.py
@@ -1,21 +1,12 @@
-def roman_to_arabic(roman_numeral):
-    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    arabic_number = 0
-    prev_value = 0
+from random import randint
 
-    for char in reversed(roman_numeral):
-        if char not in roman_values:
-            return "Ошибка: Недопустимый символ"
-
-        current_value = roman_values[char]
-        if current_value < prev_value:
-            arabic_number -= current_value
-        else:
-            arabic_number += current_value
-        prev_value = current_value
-
-    return arabic_number
+def rand_list(arr):
+    n = randint(0, len(arr) - 1)
+    deleted_num = arr.pop(n)
+    return deleted_num
 
 if __name__ == '__main__':
-    roman_numeral = input("Введите римскую цифру (число): ")
-    print(roman_to_arabic(roman_numeral))
+    a = [randint(0, 1001) for _ in range(10)]
+    a.sort()
+    deleted_num = rand_list(a)
+    print(f'Начальный массив: {a} \nУдаленное число: {deleted_num}')
